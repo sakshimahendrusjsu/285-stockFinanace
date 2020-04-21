e72fad321514de4e4d9b5390cad1d99a8ad1a7ff
sakshimahendrusjsu <sakshi.mahendru@sjsu.edu>
2020-04-20 17:14:14 -0700

    added symbols

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import requests
 import json
 import datetime
 
@@ -48,10 +49,20 @@ def index():
             value_change = stock_data['historical'][0]['change']
             perc_change = stock_data['historical'][0]['changePercent']
             now = datetime.datetime.today() 
+            if (value_change > 0):
+                format_value_change = "+" + str(value_change)
+            else:
+                format_value_change = value_change    
+            
             format_stock_symbol = "(" + stock_symbol + ")"
-            format_perc_change = "(" + str(perc_change) + ")"
+
+            if(perc_change > 0):
+                format_perc_change = "(+" + str(perc_change) + "%)"
+            else:
+                format_perc_change = "(" + str(perc_change) + "%)"
+
             tempData ={ 'tickerSymbol': format_stock_symbol,'full_name':full_name,
-            'stock_price':stock_price,'value_change':value_change,
+            'stock_price':stock_price,'value_change':format_value_change,
             'perc_change':format_perc_change,'weekday':now.strftime("%a"),'month':now.strftime("%b"),
             'day':now.strftime("%d"),'time':now.strftime("%X")+" PDT",'year':now.strftime("%Y")}
             return render_template('home.html',**tempData)
